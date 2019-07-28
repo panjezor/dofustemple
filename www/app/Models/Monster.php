eb53533643948f006c2c365a7f0c8ac251f4334c
panjezor <filipjancieslik@gmail.com>
2019-07-28 22:56:03 +0100

    Ochre project is finished and working.

--- www/app/Models/Monster.php
+++ www/app/Models/Monster.php
@@ -2,9 +2,99 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class Monster extends Model
 {
-    //
+
+    /*
+     * types:
+     * 0 = normal
+     * 1 = arch
+     * 2 = boss
+     * monsters with no arch will have a type but no associate
+     */
+    protected $fillable = ['monster_name', 'type', 'associate'];
+    public $timestamps = false;
+
+
+    public function isNormal()
+    {
+        if ($this->type === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function isArch()
+    {
+        if ($this->type === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function isBoss()
+    {
+        if ($this->type === 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function allNormals()
+    {
+        return Monster::where('type', '=', '0');
+    }
+
+    public static function allArchs()
+    {
+        return Monster::where('type', '=', '1')->orderBy('monster_name')->get();
+    }
+
+    public static function allBosses()
+    {
+        return Monster::where('type', '=', '2');
+    }
+
+    public function getType()
+    {
+        switch ($this->type) {
+            case '0':
+                return 'Normal';
+                break;
+            case '1':
+                return 'Archmonster';
+                break;
+            case '2':
+                return 'Boss';
+                break;
+        }
+    }
+
+    public function otherName($int)
+    {
+        if ($this->associate !== null) {
+            $monsters = Monster::find($this->associate);
+
+            return $monsters->monster_name;
+        } else {
+            return 'no name';
+        }
+    }
+
+    public function amountOwnedBy(User $user = null)
+    {
+        if ($user) {
+            return MonsterOwnership::where('user_id', $user->id)->where('monster_id', $this->id)->count();
+        } else {
+            return MonsterOwnership::where('user_id', 1)->where('monster_id', $this->id)->count();
+        }
+    }
+
+
 }
---
+++ www/resources/views/helperviews/datatables.blade.php
@@ -0,0 +1,2 @@
+<script type="text/javascript" src="{{ asset('js/datatables.min.js') }}" defer></script>
+<link rel="stylesheet" type="text/css" href="{{ asset('css/datatables.min.css') }}"/>
