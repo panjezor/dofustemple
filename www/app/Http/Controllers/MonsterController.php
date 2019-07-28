e42e30d1da0199a35992c72f019379e1eabbfda0
panjezor <filipjancieslik@gmail.com>
2019-07-28 12:40:37 +0100

    v0.1.0

--- www/app/Http/Controllers/MonsterController.php
+++ www/app/Http/Controllers/MonsterController.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Monster;
 use Illuminate\Http\Request;
 
 class MonsterController extends Controller
 {
-    public function add(Request $request){
-
+    public function add(Request $request)
+    {
+        $monstername = $request->input('name');
+        $type = $request->input('type');
+        $parentname = $request->input('parent');
+        Monster::firstOrCreate();
     }
-    public function input(){
+
+    public function input()
+    {
         return view('monsterinput');
     }
 }
