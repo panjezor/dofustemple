af5fc3ad1bb4770a5e0ae71992efb9860881b22d
panjezor <filipjancieslik@gmail.com>
2019-10-08 23:47:28 +0100

    changing the way that drop calculator works on js side. still in development phase, got the major function sorted, no display finished yet.

--- www/public/js/drop-index.js
+++ www/public/js/drop-index.js
@@ -1,35 +1,33 @@
 $().ready(function () {
+    $('#char-span1').css('display', 'inline-block');
+
+
     $('#character-count').on('change', function () {
             count = $(this).val();
-            $('#character-pp').text('');
-            let options = '';
-            for (let i = 0; i < count; i++) {
-                options += '<label for="character' + (i + 1) + '">Character</label><input id="character' + (i + 1) + '" class="form-control">';
-            }
-            options += '';
-            $('#character-pp').append(options);
-        }
-    );
-    $('#monster-count').on('change', function () {
-            count = $(this).val();
-            $('#monster-pp').text('');
-            let options = '';
-            for (let i = 0; i < count; i++) {
-                options += '<label for="monster' + (i + 1) + '">Monster</label><input id="monster' + (i + 1) + '" class="form-control">';
-            }
-            options += '';
-            $('#monster-pp').append(options);
-        }
-    );
+        for (let i = 1; i < 9; i++) {
+            $('#char-span' + i).css('display', 'none');
 
+            }
+        for (let i = 1; i < parseInt(count) + 1; i++) {
+            $('#char-span' + i).css('display', 'inline-block');
+
+        }
+        }
+    );
 });
 
 function countChance() {
-    let characters = $('#character-pp>.form-control');
-    let monsterProb = $('#monster-pp').val();
-    let chance = 1;
-    characters.each(function (element) {
-        console.log(element);
+    let chars = [];
+    for (let i = 1; i < parseInt($('#character-count').val()) + 1; i++) {
+        chars.push(parseInt($('#character' + i).val()) / 100);
+    }
+    //prospecting of all the characters is now in chars[]
+    // for each one of them we need to count the chance
+    let totalChance = 1;
+    let dropRate = parseFloat($('#monster-drop').val()) / 100;
+    $(chars).each(function (index, element) {
+        totalChance *= 1 - (element * dropRate);
     });
-    chance = 'hehe';
+    let pt = 1 - totalChance;
+    console.log(pt.toFixed(4) * 100 + '%');
 }
