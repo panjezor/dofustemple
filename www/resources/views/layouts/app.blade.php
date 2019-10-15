c86f96c469e19e75de28506fd4d2063dff822ea9
panjezor <filipjancieslik@gmail.com>
2019-10-15 18:22:41 +0100

    bugfix for monsters datatables search.

--- www/resources/views/layouts/app.blade.php
+++ www/resources/views/layouts/app.blade.php
@@ -143,7 +143,7 @@
                 <div class="container-fluid text-center text-md-left">
                     <div class="row">
                         <div class="col-md-6 mt-md-0 mt-3">
-                            <h5 class="text-uppercase">{{__('main.development')}}</h5>
+                            <h5 class="text-uppercase"><a href="/to-do">{{__('main.development')}}</a></h5>
                             <p></p>
                         </div>
                         <hr class="clearfix w-100 d-md-none pb-3">
--- www/resources/views/to-do.blade.php
+++ www/resources/views/to-do.blade.php
@@ -1,13 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-
+    <div class="col-4">
+        <div class="card">
+            <div class="card-header">{{__('future-features')}}</div>
+            <div class="card-body">
+                <ol>
+                    @php($todos = Todo::all())
+                    @foreach()
+                </ol>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @push('scripts')
-
-@endpush
-
-@push('styles')
 
 @endpush
