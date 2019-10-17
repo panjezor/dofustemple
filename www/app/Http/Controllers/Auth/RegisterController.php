ab3110559f44d2a3ad466040064f071c1cfe3afc
panjezor <filipjancieslik@gmail.com>
2019-10-17 19:16:00 +0100

    changed so that the footer is now fixed to the bottom and registration moves the user to main page.

--- www/app/Http/Controllers/Auth/RegisterController.php
+++ www/app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Models\User;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -59,7 +59,7 @@ class RegisterController extends Controller
      * Create a new user instance after a valid registration.
      *
      * @param  array  $data
-     * @return \App\Models\User
+     * @return User
      */
 
     protected function create(array $data)
--- www/resources/views/layouts/app.blade.php
+++ www/resources/views/layouts/app.blade.php
@@ -123,7 +123,7 @@
                     </div>
                 </div>
             </main>
-            <footer class="page-footer font-small pt-4">
+            <footer class="fixed-bottom page-footer font-small pt-4">
                 <div class="container-fluid text-center text-md-left">
                     <div class="row">
                         <div class="col-md-6 mt-md-0 mt-3">
