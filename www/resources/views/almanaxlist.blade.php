3ffcb79d66974623edd402cbddb467e65c360e05
panjezor <filipjancieslik@gmail.com>
2020-04-09 00:42:25 +0100

    im an idiot.

--- www/resources/views/almanaxlist.blade.php
+++ www/resources/views/almanaxlist.blade.php
@@ -6,27 +6,27 @@
 @endsection
 @push('scripts')
     <script>
-        $().ready();
-        {
-            for (var i = 0; i < 365; i++) {
-                var date = new Date();
-                date.setDate(date.getDate() + i);
-                var mon = date.getMonth() + 1;
-                var futDate = date.getFullYear() + "-";
-                if (mon < 10) {
-                    futDate += "0" + mon + "-";
-                } else {
-                    futDate += mon + "-";
-                }
-                if (date.getDate() < 10) {
-                    futDate += "0" + date.getDate();
-                } else {
-                    futDate += +date.getDate();
-                }
-                var link = "/almanax/" + futDate;
-                $('#holder').append('<a href="' + link + '">click</a><br>');
+        $().ready(function () {
+                for (var i = 0; i < 365; i++) {
+                    var date = new Date();
+                    date.setDate(date.getDate() + i);
+                    var mon = date.getMonth() + 1;
+                    var futDate = date.getFullYear() + "-";
+                    if (mon < 10) {
+                        futDate += "0" + mon + "-";
+                    } else {
+                        futDate += mon + "-";
+                    }
+                    if (date.getDate() < 10) {
+                        futDate += "0" + date.getDate();
+                    } else {
+                        futDate += +date.getDate();
+                    }
+                    var link = "/almanax/" + futDate;
+                    $('#holder').append('<a href="' + link + '">click</a><br>');
 
+                }
             }
-        }
+        );
     </script>
 @endpush
