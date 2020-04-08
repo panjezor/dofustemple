ea3ad6e1dd5528f5548f045e421dc16c8c0aa8ed
panjezor <filipjancieslik@gmail.com>
2020-04-09 00:41:39 +0100

    datatables helper for language.

--- www/resources/views/home.blade.php
+++ www/resources/views/home.blade.php
@@ -58,30 +58,7 @@
             $('#almanaxtable').DataTable(
                 {
                     "lengthMenu": [7, 14, 28],
-                    "language": {
-                        "decimal": "{{__('datatables.decimal')}}",
-                        "emptyTable": "{{__('datatables.emptyTable')}}",
-                        "info": "{{__('datatables.info',['start'=>'_START_','end'=>'_END_','total'=>'_TOTAL_'])}}",
-                        "infoEmpty": "{{__('datatables.infoEmpty')}}",
-                        "infoFiltered": "{{__('datatables.infoFiltered',['max'=>'_MAX_'])}}",
-                        "infoPostFix": "{{__('datatables.infoPostFix')}}",
-                        "thousands": "{{__('datatables.thousands')}}",
-                        "lengthMenu": "{{__('datatables.lengthMenu',['menu'=>'_MENU_'])}}",
-                        "loadingRecords": "{{__('datatables.loadingRecords')}}",
-                        "processing": "{{__('datatables.processing')}}",
-                        "search": "{{__('datatables.search')}}",
-                        "zeroRecords": "{{__('datatables.zeroRecords')}}",
-                        "paginate": {
-                            "first": "{{__('datatables.first')}}",
-                            "last": "{{__('datatables.last')}}",
-                            "next": "{{__('datatables.next')}}",
-                            "previous": "{{__('datatables.previous')}}"
-                        },
-                        "aria": {
-                            "sortAscending": "{{__('datatables.sortAscending')}}",
-                            "sortDescending": "{{__('datatables.sortDescending')}}",
-                        }
-                    },
+                    @include('helpers.datatables.languagepack')
 
                 }
             );
