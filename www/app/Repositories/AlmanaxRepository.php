9298afe608ca39090195d06934f6d2789d026916
panjezor <filipjancieslik@gmail.com>
2020-04-12 23:18:49 +0100

    pickNext now uses unified "today" date.

--- www/app/Repositories/AlmanaxRepository.php
+++ www/app/Repositories/AlmanaxRepository.php
@@ -7,12 +7,13 @@ namespace App\Repositories;
 use App\Models\Almanax;
 use App\Repositories\Interfaces\AlmanaxRepositoryInterface;
 use Carbon\Carbon;
-use Carbon\CarbonInterval;
 use DateTimeZone;
 use Illuminate\Database\Eloquent\Collection;
 
 class AlmanaxRepository implements AlmanaxRepositoryInterface
 {
+
+    const TIMEZONE = 'Europe/Paris';
 
     public function all(): Collection
     {
@@ -21,11 +22,10 @@ class AlmanaxRepository implements AlmanaxRepositoryInterface
 
     public function pickNext($days = 28): Collection
     {
-        $date = Carbon::today(new DateTimeZone('Europe/Paris'));
-        $interval = CarbonInterval::days($days);
+        $date = Carbon::today(new DateTimeZone(self::TIMEZONE));
         return Almanax::
-        whereDate('needed_by', '>', Carbon::yesterday(new DateTimeZone('Europe/Paris'))->toDateString())->
-        whereDate('needed_by', '<', $date->add($interval)->toDateTimeString())->
+        whereDate('needed_by', '>', ($date->subDay()->toDateString()))->
+        whereDate('needed_by', '<', ($date->addDays($days + 1)->toDateString()))->
         get();
     }
 }
