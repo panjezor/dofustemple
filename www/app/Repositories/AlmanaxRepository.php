0e10ec3f180abdbce67f9b82282d0ee57971d97d
panjezor <filipjancieslik@gmail.com>
2020-04-12 23:09:59 +0100

    added timezone support for DGT

--- www/app/Repositories/AlmanaxRepository.php
+++ www/app/Repositories/AlmanaxRepository.php
@@ -8,6 +8,7 @@ use App\Models\Almanax;
 use App\Repositories\Interfaces\AlmanaxRepositoryInterface;
 use Carbon\Carbon;
 use Carbon\CarbonInterval;
+use DateTimeZone;
 use Illuminate\Database\Eloquent\Collection;
 
 class AlmanaxRepository implements AlmanaxRepositoryInterface
@@ -20,10 +21,10 @@ class AlmanaxRepository implements AlmanaxRepositoryInterface
 
     public function pickNext($days = 28): Collection
     {
-        $date = Carbon::today();
+        $date = Carbon::today(new DateTimeZone('Europe/Paris'));
         $interval = CarbonInterval::days($days);
         return Almanax::
-        whereDate('needed_by', '>', Carbon::yesterday()->toDateString())->
+        whereDate('needed_by', '>', Carbon::yesterday(new DateTimeZone('Europe/Paris'))->toDateString())->
         whereDate('needed_by', '<', $date->add($interval)->toDateTimeString())->
         get();
     }
