1adcf78b562c80e5001a07b5e8da430a73c9aa8e
panjezor <filipjancieslik@gmail.com>
2020-04-16 23:17:30 +0100

    added extra lang row.

--- www/resources/lang/en/drop.php
+++ www/resources/lang/en/drop.php
@@ -8,6 +8,7 @@ return [
     'item-drop-rate' => 'Item drop rate (in percentage)',
     'prospecting-lock' => 'Prospecting lock',
     'monster-count' => 'Monster count',
-    'chance-explanation' => 'The chance that this team as a whole will get at least one item with that rate is equal to: '
+    'chance-explanation' => 'The chance that this team as a whole will get at least one item with that rate is equal to: ',
+    'result' => 'Result'
 
 ];
