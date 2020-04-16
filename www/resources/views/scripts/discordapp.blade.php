7018db034f872784d2fa7887ff7d5aecd7fb48f9
panjezor <filipjancieslik@gmail.com>
2020-04-16 23:18:39 +0100

    added new discord app widget on every page.

---
+++ www/resources/views/scripts/discordapp.blade.php
@@ -0,0 +1,12 @@
+<meta name="discord-polish" content="577232134318850049">
+<meta name="discord-english" content="700462196479623288">
+<script src="https://cdn.jsdelivr.net/npm/@widgetbot/crate@3" async defer>
+    new Crate({
+        server: '576376522957651988',
+        channel: $('html')[0].lang == 'pl' ? $('meta[name=discord-polish]')[0].content : $('meta[name=discord-english]')[0].content,
+        shard: 'https://e.widgetbot.io',
+        indicator: true,
+        notifications: true,
+        color: 'darkgreen'
+    })
+</script>
